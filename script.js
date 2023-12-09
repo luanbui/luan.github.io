48f78c25999c05dd1d5bced84644640b73f69aff
luanbui <luankinhbui@gmail.com>
2023-12-08 23:56:45 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function showAlert() {
+    alert('Hello! My name is Luan Bui.');
+}
+
+function toggleExperience() {
+    var experienceSection = document.getElementById('experience');
+    experienceSection.classList.toggle('hidden');
+}
+
+function toggleAboutAnimation() {
+    var aboutSection = document.getElementById('about');
+    aboutSection.classList.toggle('animated');
+}
